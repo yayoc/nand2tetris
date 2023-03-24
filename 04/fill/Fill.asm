bf39b1a56871b4e51f281af37bff5411b22f7e99
yayoc <hi@yayoc.com>
2023-03-25 04:56:09 +0900

    implement Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,76 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@8192
+D=A
+@n
+M=D // n = 8192
+
+@SCREEN
+D=A
+@addr
+M=D
+
+(LOOP)
+    @KBD
+    D=M
+
+    // i = 0
+    @i
+    M=0
+
+    @BLACKEN
+    D;JNE
+
+    @WHITEN
+    D;JEQ
+
+    @LOOP
+    0;JMP
+
+(BLACKEN)
+    @n
+    D=M
+    @i
+    D=D-M
+
+    @END
+    D;JEQ
+
+    // blacken
+    @addr
+    D=M
+    @i
+    A=D+M
+    M=-1
+
+    @i
+    M=M+1 // i++
+
+    @BLACKEN
+    0;JMP
+
+(WHITEN)
+    @n
+    D=M
+    @i
+    D=D-M
+
+    @END
+    D;JEQ
+
+    @addr
+    D=M
+    @i
+    A=D+M
+    M=0
+
+    @i
+    M=M+1 // i++
+
+    @WHITEN
+    0;JMP
+
+(END)
+    @LOOP
+    0;JMP
